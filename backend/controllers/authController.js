7071e05491dcdcaa8a1db844f747cc0e349029d8
Swechhya <swechhhya.stha@gmail.com>
2025-07-02 15:36:11 +0545

    Update3

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -18,7 +18,7 @@ const registerUser = async (req, res) => {
         // Check if user already exists
         const userExists = await User.findOne({ email });
         if(userExists){
-            return res.status(400),json({ message: "User already exists" });
+            return res.status(400).json({ message: "User already exists" });
         }
 
         // Determine user role: Admin if correct token is provided, otherwise Member 
@@ -62,6 +62,28 @@ const registerUser = async (req, res) => {
 // @access Public
 const loginUser = async (req, res) => {
      try{
+        const{  email , password } = req.body;
+
+        const user =  await User.findOne({email});
+        if(!user){
+            return  res.status(401).json({ message : "Invalid email or password"});
+        }
+
+        // Compare password
+        const isMatch =  await bcrypt.compare( password, user.password);
+        if(!isMatch){
+            return res.status(401).json({ message : "Invalid email or password" });
+        }
+
+        //  Return user data with JWT
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,        
+            role: user.role,
+            profileImageUrl: user.profileImageUrl,
+            token: generateToken(user._id),
+        })
     } catch(error){
         res.status(500).json({ message : "Server error", error: error.message});
     }
@@ -72,6 +94,11 @@ const loginUser = async (req, res) => {
 // @access Private(Requires JWT)
 const getUserProfile = async (req, res) => {
      try{
+        const user = await User.findById(req.user.id).select("-password");
+        if (!user){
+            return res.status(404).json({ message : "User not found"});
+        }
+        res.json(user);
     } catch(error){
         res.status(500).json({ message : "Server error", error: error.message});
     }
@@ -82,6 +109,30 @@ const getUserProfile = async (req, res) => {
 // @access Private(Requires JWT)
 const updateUserProfile = async (req, res) => {
      try{
+        const user = await User.findById(req.res.id);
+
+        if(!user){
+            return res.status(404).json({ message : "User not found"});
+        }
+
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if(req.body.password){
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(req.body.password,salt);
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+              _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,        
+            role: updatedUser.role,
+            profileImageUrl: updatedUser.profileImageUrl,
+            token: generateToken(updatedUser._id),
+        })
     } catch(error){
         res.status(500).json({ message : "Server error", error: error.message});
     }
