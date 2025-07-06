74c6b4e6a47c4af1daceb5a0bd0b9aa31e4b5e93
Jenisha781 <karmacharyajenisha781@gmail.com>
2025-07-06 13:32:30 +0545

    Update MyTasks.jsx

--- frontend/src/pages/User/MyTasks.jsx
+++ frontend/src/pages/User/MyTasks.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
 import { useNavigate } from "react-router-dom";
-import axiosInstance from "../../utils/axiosInstance";
+import axiosInstance from '/src/utils/axiosInstance.js';
 import { API_PATHS } from "../../utils/apiPaths";
 import { LuFileSpreadsheet } from "react-icons/lu";
 import TaskStatusTabs from "../../components/TaskStatusTabs";
