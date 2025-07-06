eed1a1beb79d0601e9fc71d3f585bc409cdf3743
Jenisha781 <karmacharyajenisha781@gmail.com>
2025-07-06 13:33:18 +0545

    Update ViewTaskDetails.jsx

--- frontend/src/pages/User/ViewTaskDetails.jsx
+++ frontend/src/pages/User/ViewTaskDetails.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import axiosInstance from '../../utils/axiosInstance';
+import axiosInstance from '/src/utils/axiosInstance.js';
 import { API_PATHS } from '../../utils/apiPaths';
 import DashboardLayout from '../../components/layouts/DashboardLayout';
 import AvatarGroup from '../../components/AvatarGroup';
