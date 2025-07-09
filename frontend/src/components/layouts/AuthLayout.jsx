6211b959e109b8d9769b2cc881c3723dcbc2302a
Sanam-Shrestha04 <sthasanam067@gmail.com>
2025-07-09 13:42:54 +0545

    Added image

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -2,21 +2,26 @@ import React from 'react';
 import UI_IMG from '../../assets/images/bg-img.png';
 
 const AuthLayout = ({ children }) => {
-  
   return (
-    <div
-      className="w-screen h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat"
-      style={{
-        backgroundImage: `url("${UI_IMG}")`,
-      }}
-    >
-      <div className="bg-white bg-opacity-80 rounded-lg p-8 w-[90%] md:w-[60vw] h-[90%] overflow-auto shadow-lg z-10">
-        <h2 className="text-xl font-semibold text-black mb-4">Task Manager</h2>
-        {children}
+    <div className="flex w-screen h-screen">
+      {/* Left: Login Form */}
+      <div className="flex-1 flex items-center justify-center px-6 md:px-12">
+        <div className="w-full max-w-md">
+          {children}
+        </div>
       </div>
+
+      {/* Right: Background Image */}
+      <div
+        className="hidden md:block sm:w-[40vw] 
+    md:w-[35vw] 
+    lg:w-[45vw] 
+    xl:w-[50vw]
+opacity-85 h-screen bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${UI_IMG})` }}
+      />
     </div>
   );
 };
 
 export default AuthLayout;
-
