5df2bc5aa926174d4fcd49c9ab2b0ba8dade97a5
Swechhya <swechhhya.stha@gmail.com>
2025-09-02 00:00:40 +0545

    MT

--- frontend/src/pages/Admin/ManageTasks.jsx
+++ frontend/src/pages/Admin/ManageTasks.jsx
@@ -5,7 +5,7 @@ import axiosInstance from "../../utils/axiosInstance.js";
 import { API_PATHS } from "../../utils/apiPaths";
 import { LuFileSpreadsheet } from "react-icons/lu";
 import TaskStatusTabs from "../../components/TaskStatusTabs";
-import TaskCard from "../../components/Cards/TaskCard";
+import TaskCard from "../../components/Cards/TaskCard.jsx";
 import toast from "react-hot-toast";
 
 const ManageTasks = () => {
